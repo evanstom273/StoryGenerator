530cc2a89ce80c1592ac5c6383c537522bdeda47
evanstom273 <60609303+evanstom273@users.noreply.github.com>
2026-09-25 16:04:04 +0100

    Remove unused button import

--- src/app/layout/V2Shell.tsx
+++ src/app/layout/V2Shell.tsx
@@ -4,7 +4,6 @@ import { BrandMark } from "../../components/BrandMark";
 import { AnimatedOutlet } from "../../components/ui/AnimatedOutlet";
 import { DRAWER_PANEL_CLASS, OVERLAY_BACKDROP_CLASS } from "../ui/motion";
 import { SearchIcon } from "../../components/icons";
-import { Button } from "../../components/ui/Button";
 import { MetaChatOverlay } from "../../components/story/MetaChatOverlay";
 import { cn } from "../../utils/cn";
 import { META_CHAT_OPEN_STORAGE_KEY } from "../../lib/jobNotifications";
